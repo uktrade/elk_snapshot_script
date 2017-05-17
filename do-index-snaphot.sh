52f4767834e4f416a4ac5d8e511f4a796bc25bdb
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2017-05-17 16:47:03 +0100

    Added silent curl

--- do-index-snaphot.sh
+++ do-index-snaphot.sh
@@ -16,7 +16,7 @@ EOF
 if [ $(curl -s https://localhost:9200/_snapshot/s3_repository/ -k --user elastic:changeme | jq '.[]'|tail -1) == "404" ]
 then
 	echo "Enabling snapshots to S3"
-	/usr/bin/curl -XPUT 'https://localhost:9200/_snapshot/s3_repository?verify=false&pretty' -H 'Content-Type: application/json' -d "$request_body" -k --user elastic:changeme
+	/usr/bin/curl -s -XPUT 'https://localhost:9200/_snapshot/s3_repository?verify=false&pretty' -H 'Content-Type: application/json' -d "$request_body" -k --user elastic:changeme
 	echo "Creating an index snapshot"
 	/usr/bin/curl -s -XPUT "https://localhost:9200/_snapshot/s3_repository/`date +%d-%m-%Y_%H:%M`" -k --user elastic:changeme
 	exit 0
